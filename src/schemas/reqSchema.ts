6163590506cf5a586eb68918382001f1fd3a627b
Lucas Lima <lucas.lima.psico@hotmail.com>
2023-09-07 00:27:56 -0300

    fix: schema

--- src/schemas/reqSchema.ts
+++ src/schemas/reqSchema.ts
@@ -2,13 +2,6 @@ const { z } = require('zod')
 
 exports.getTableSchema = z
     .object({
-        serie: z
-            .string({
-                required_error: 'serie é obrigatória',
-                invalid_type_error: 'serie inválida',
-            })
-            .min(1)
-            .max(1)
-            .trim(),
+        serie: z.union([z.literal('a'), z.literal('b')], { errorMap: () => ({ message: 'Série inválida' }) }),
     })
     .strict()
